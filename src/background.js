c99a9512e89fe954ce6b651488c29f3b1d768b22
MrThanlon <i@anclx.cc>
2022-05-16 21:55:32 +0800

    add: save data to sqlite database
    Signed-off-by: MrThanlon <i@anclx.cc>

--- src/background.js
+++ src/background.js
@@ -1,12 +1,25 @@
 'use strict'
 
-import { app, protocol, BrowserWindow, ipcMain } from 'electron'
+import { app, protocol, BrowserWindow, ipcMain, Menu, dialog } from 'electron'
 import { createProtocol } from 'vue-cli-plugin-electron-builder/lib'
 import installExtension, { VUEJS3_DEVTOOLS } from 'electron-devtools-installer'
 import { spawn } from 'child_process'
 import shellParser from 'shell-parser'
 import readline from 'readline'
+import sqlite3 from 'sqlite3'
+
 const isDevelopment = process.env.NODE_ENV !== 'production'
+
+let db = new sqlite3.Database(':memory:')
+let insertStatement = null
+db.serialize(() => {
+  db.run('CREATE TABLE data (time UNSIGNED BIG INT NOT NULL, tag TEXT NOT NULL, value REAL NOT NULL)')
+  db.run('CREATE INDEX time ON data (time)')
+  db.run('CREATE INDEX tag ON data (tag)')
+  insertStatement = db.prepare('INSERT INTO data (time, tag, value) VALUES (?, ?, ?)')
+})
+let dbBleedingData = []
+let dbBleeding = false
 
 // Scheme must be registered before the app is ready
 protocol.registerSchemesAsPrivileged([
@@ -57,12 +70,22 @@ ipcMain.handle('addSource', (event, cmd) => {
   rl.on('line', line => {
     // TODO: parse and tell render process
     const dateStr = dateFormat(new Date())
+    const now = Date.now()
     const regex = /(?:([a-z_-]*)[^a-z0-9_-]+)?(-?\d+(?:\.\d*)?)/ig
     const result = []
     let m
     let idx = 1
     while ((m = regex.exec(line)) !== null) {
-      result.push({ tag: m[1] || `channel ${idx}`, data: parseFloat(m[2]) })
+      const tag = m[1] || `channel ${idx}`
+      const data = parseFloat(m[2])
+      // insert db
+      if (dbBleeding) {
+        dbBleedingData.push({ time: now, tag, value: data })
+      } else {
+        insertStatement.run(now, tag, data)
+      }
+      // to window
+      result.push({ tag, data })
       idx += 1
     }
     mainWindow.webContents.send('data', {
@@ -89,6 +112,83 @@ ipcMain.handle('sendSource', (event, to, message) => {
 })
 
 async function createWindow () {
+  // menu
+  const menuTemplate = [
+    {
+      role: 'appMenu'
+    },
+    {
+      role: 'fileMenu',
+      submenu: [
+        {
+          label: 'Open... (TODO)',
+          accelerator: 'CommandOrControl+O',
+          enabled: false,
+          async click () {
+            const result = await dialog.showOpenDialog(mainWindow, {
+              properties: [
+                'openFile', 'showHiddenFiles', 'createDirectory',
+                'promptToCreate'
+              ]
+            })
+            if (result.canceled) {
+              return
+            }
+            const filePath = result.filePaths[0]
+            console.debug(filePath)
+          }
+        },
+        {
+          label: 'Save',
+          accelerator: 'CommandOrControl+S',
+          async click () {
+            const result = await dialog.showSaveDialog(mainWindow, {
+              properties: ['showHiddenFiles', 'createDirectory', 'showOverwriteConfirmation']
+            })
+            if (result.canceled) {
+              return
+            }
+            const filePath = result.filePath
+            const backup = db.backup(filePath)
+            backup.step(-1, e => {
+              if (e) throw e
+              backup.finish(e => {
+                if (e) throw e
+                // saved
+                db.close()
+                db = new sqlite3.Database(filePath)
+                insertStatement = db.prepare('INSERT INTO data (time, tag, value) VALUES (?, ?, ?)', e => {
+                  // write bleeding data
+                  dbBleedingData.forEach(({ time, tag, value }) => {
+                    insertStatement.run(time, tag, value)
+                  })
+                  dbBleeding = false
+                })
+              })
+            })
+            dbBleeding = true
+          }
+        },
+        {
+          label: 'Append saving (TODO)',
+          enabled: false
+        }
+      ]
+    },
+    {
+      role: 'editMenu'
+    },
+    {
+      role: 'viewMenu'
+    },
+    {
+      role: 'windowMenu'
+    },
+    {
+      role: 'help'
+    }
+  ]
+  Menu.setApplicationMenu(Menu.buildFromTemplate(menuTemplate))
   // Create the browser window.
   mainWindow = new BrowserWindow({
     width: 800,
